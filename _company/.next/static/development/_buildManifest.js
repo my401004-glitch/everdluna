1c2fc2ea2d3271fc763a2bff92573e8e6b4815c2
my401004-glitch <285446612+my401004-glitch@users.noreply.github.com>
2026-05-20 20:40:35 +0900

    Auto-sync local brain

--- _company/.next/static/development/_buildManifest.js
+++ _company/.next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/challenge":["static\u002Fchunks\u002Fpages\u002Fchallenge.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fchallenge"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
